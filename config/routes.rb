e7b6336d3e7671455ec76477f94b8c2904f5e6c3
ravi <chiklu.ravi@gmail.com>
2015-04-06 14:53:52 +0530

    Added the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
   Rails.application.routes.draw do
 
+  get 'about'=>'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +8,7 @@
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
